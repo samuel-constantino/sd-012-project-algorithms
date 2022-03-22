f72e670b0c7bd6f12c54dc41c68bc0779a86e3f6
Samuel Constantino <samuelicapuidados@gmail.com>
2022-03-22 18:37:03 -0300

    altera nome de variavel

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,10 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    repeat_students = 0
+    count = 0
     try:
         for student_period in permanence_period:
             if student_period[0] <= target_time <= student_period[1]:
-                repeat_students += 1
+                count += 1
     except TypeError:
         return None
 
-    return repeat_students
+    return count
