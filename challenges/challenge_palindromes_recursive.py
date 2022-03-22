1e54b2f9cc9b5745b588fb15af783de9ba83f3c1
Samuel Constantino <samuelicapuidados@gmail.com>
2022-03-22 12:22:21 -0300

    implementa requisito 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,17 @@
-def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+def reverse(word):
+    if len(word) < 2:
+        return word
+
+    return reverse(word[1:]) + word[0]
+
+
+def is_palindrome_recursive(word, _low_index, _high_index):
+    if word == '':
+        return False
+
+    word_reverse = reverse(word)
+
+    if word == word_reverse:
+        return True
+
+    return False
