e415c7506d97513d808fecb181ca8a05d2273c68
Samuel Constantino <samuelicapuidados@gmail.com>
2022-03-22 19:03:12 -0300

    refatora req 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,34 +1,3 @@
-def merge_sort(array):
-    if len(array) <= 1:
-        return array
-
-    mid = len(array) // 2
-
-    left, right = merge_sort(array[:mid]), merge_sort(array[mid:])
-
-    return merge(left, right, array.copy())
-
-
-def merge(left, right, merged):
-    left_cursor, right_cursor = 0, 0
-
-    while left_cursor < len(left) and right_cursor < len(right):
-        if left[left_cursor] <= right[right_cursor]:
-            merged[left_cursor + right_cursor] = left[left_cursor]
-            left_cursor += 1
-        else:
-            merged[left_cursor + right_cursor] = right[right_cursor]
-            right_cursor += 1
-
-    for left_cursor in range(left_cursor, len(left)):
-        merged[left_cursor + right_cursor] = left[left_cursor]
-
-    for right_cursor in range(right_cursor, len(right)):
-        merged[left_cursor + right_cursor] = right[right_cursor]
-
-    return merged
-
-
 def verify_duplicate(sorted_nums):
     for i in range(len(sorted_nums)):
         if sorted_nums[i] < 0:
@@ -38,9 +7,9 @@ def verify_duplicate(sorted_nums):
 
 
 def find_duplicate(nums):
-    sorted_nums = merge_sort(nums)
+    # sorted_nums = merge_sort(nums)
     try:
-        return verify_duplicate(sorted_nums)
+        return verify_duplicate(sorted(nums))
     except IndexError:
         return False
     except TypeError:
--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,14 +1,5 @@
-def reverse(word):
-    word_reverse = []
-    for c in word:
-        word_reverse.insert(0, c)
-    return "".join(word_reverse)
-
-
 def is_palindrome_iterative(word):
     if word == '':
         return False
-
-    # word_reverse = reverse(word)
 
     return word == word[::-1]
