a3b8b3560af069d27c92cbb506431b930bc4d07d
Samuel Constantino <samuelicapuidados@gmail.com>
2022-03-22 17:20:03 -0300

    refatora complexidade do requisito 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -29,14 +29,18 @@ def merge(left, right, merged):
     return merged
 
 
+def verify_duplicate(sorted_nums):
+    for i in range(len(sorted_nums)):
+        if sorted_nums[i] < 0:
+            return False
+        if sorted_nums[i] == sorted_nums[i + 1]:
+            return sorted_nums[i]
+
+
 def find_duplicate(nums):
     sorted_nums = merge_sort(nums)
     try:
-        for i in range(len(sorted_nums)):
-            if sorted_nums[i] < 0:
-                return False
-            if sorted_nums[i] == sorted_nums[i + 1]:
-                return sorted_nums[i]
+        return verify_duplicate(sorted_nums)
     except IndexError:
         return False
     except TypeError:
